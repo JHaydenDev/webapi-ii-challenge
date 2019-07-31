3106bc1070e4a85b7663e29d4f11a06e24c34532
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-07-31 13:48:00 -0500

    Got dependancies and router working

---
+++ data/posts/postsRouter.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const posts = require("../db");
+const router = express.Router();
+
+//Server.Get posts
+router.get("/", (req, res) => {
+ 
+});
+
+//Server.Get post:id
+router.get("/:id", (req, res) => {
+ 
+});
+
+//Server.Get comments
+router.get("/:id/comments", (req, res) => {
+ 
+});
+
+//Server.post
+router.post("/", (req, res) => {
+ 
+});
+
+//Server.post comments
+router.post("/:id/comments", (req, res) => {
+ 
+});
+
+//Server.put posts with id
+router.put("/:id", (req, res) => {
+});
+
+//Server.delete posts
+router.delete("/:id", (req, res) => {
+
+});
+
+module.exports = router;
