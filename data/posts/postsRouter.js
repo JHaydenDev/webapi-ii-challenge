aae01c46435c30d537317fab7b88560af201ed36
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-07-31 14:25:30 -0500

    Post endpoints made.

--- data/posts/postsRouter.js
+++ data/posts/postsRouter.js
@@ -20,7 +20,7 @@ router.get("/", (req, res) => {
     });
 });
 
-//Server.Get post:id
+//Server.Get post:id and comments,  Combined to make code cleaner
 router.get("/:id/comments", async (req, res) => {
   const post_id = req.params.id;
 
@@ -49,16 +49,147 @@ router.get("/:id/comments", async (req, res) => {
   }
 });
 
-//Server.post
-router.post("/", (req, res) => {});
+//Server.post 
+router.post('/', (req, res) => {
+    const post = req.body;
+  
+    if (!post.title || !post.contents) {
+      const missingField =
+        !post.title && !post.contents
+          ? 'a title and contents'
+          : !post.title
+          ? 'a title'
+          : 'contents';
+  
+      res.status(400).json({
+        success: false,
+        errorMessage: `Please provide ${missingField} for the post.`
+      });
+    } else {
+      Posts.insert(post)
+        .then(post => {
+          res.status(201).json({
+            success: true,
+            post
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            success: false,
+            errorMessage:
+              'There was an error while save the post to the database',
+            err
+          });
+        });
+    }
+  });
 
-//Server.post comments
-router.post("/:id/comments", (req, res) => {});
+//Server.post id: comments
+router.post("/:id/comments", async (req, res) => {
+  const text = req.body.text;
+  const post_id = req.params.id;
+  console.log(text);
 
-//Server.put posts with id
-router.put("/:id", (req, res) => {});
+  const post = await Posts.findById(post_id);
+
+  if (post.length === 0) {
+    res.status(404).json({
+      success: false,
+      errorMessage: "The post with the specificed ID does not exist."
+    });
+  } else if (!text) {
+    res.status(400).json({
+      success: false,
+      erroMessage: "Please provide text for the comment."
+    });
+  } else {
+    Posts.insertComment({
+      text,
+      post_id
+    })
+      .then(comment => {
+        res.status(201).json({
+          success: true,
+          comment
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the comment to the database",
+          err
+        });
+      });
+  }
+});
+
+//Server.post comments and id, combined to make code cleaner
+router.post("/:id/comments", async (req, res) => {
+  const text = req.body.text;
+  const post_id = req.params.id;
+  console.log(text);
+
+  const post = await Posts.findById(post_id);
+
+  if (post.length === 0) {
+    res.status(404).json({
+      success: false,
+      errorMessage: "The post with the specificed ID does not exist."
+    });
+  } else if (!text) {
+    res.status(400).json({
+      success: false,
+      erroMessage: "Please provide text for the comment."
+    });
+  } else {
+    Posts.insertComment({
+      text,
+      post_id
+    })
+      .then(comment => {
+        res.status(201).json({
+          success: true,
+          comment
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the comment to the database",
+          err
+        });
+      });
+  }
+});
 
 //Server.delete posts
-router.delete("/:id", (req, res) => {});
+router.delete('/:id', async (req, res) => {
+    const post_id = req.params.id;
+  
+    const post = await Posts.findById(post_id);
+  
+    if (post.length === 0) {
+      res.status(404).json({
+        success: false,
+        errorMessage: 'The post with the specificed ID does not exist.'
+      });
+    } else {
+      Posts.remove(post_id)
+        .then(post => {
+          res.status(200).json({
+            success: true,
+            post
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            success: false,
+            errorMessage: 'The post could not be removed',
+            err
+          });
+        });
+    }
+  });
+  
 
 module.exports = router;
