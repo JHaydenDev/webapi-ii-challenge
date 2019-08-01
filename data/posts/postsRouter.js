991fabc56d7f86f676841117f5ba8e5b30a7108e
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-07-31 20:21:41 -0500

    MVP for real this time.

--- data/posts/postsRouter.js
+++ data/posts/postsRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const posts = require("../db");
+const Posts = require("../db");
 const router = express.Router();
 
 //Server.Get posts
@@ -49,40 +49,40 @@ router.get("/:id/comments", async (req, res) => {
   }
 });
 
-//Server.post 
-router.post('/', (req, res) => {
-    const post = req.body;
-  
-    if (!post.title || !post.contents) {
-      const missingField =
-        !post.title && !post.contents
-          ? 'a title and contents'
-          : !post.title
-          ? 'a title'
-          : 'contents';
-  
-      res.status(400).json({
-        success: false,
-        errorMessage: `Please provide ${missingField} for the post.`
-      });
-    } else {
-      Posts.insert(post)
-        .then(post => {
-          res.status(201).json({
-            success: true,
-            post
-          });
-        })
-        .catch(err => {
-          res.status(500).json({
-            success: false,
-            errorMessage:
-              'There was an error while save the post to the database',
-            err
-          });
+//Server.post
+router.post("/", (req, res) => {
+  const post = req.body;
+
+  if (!post.title || !post.contents) {
+    const missingField =
+      !post.title && !post.contents
+        ? "a title and contents"
+        : !post.title
+        ? "a title"
+        : "contents";
+
+    res.status(400).json({
+      success: false,
+      errorMessage: `Please provide ${missingField} for the post.`
+    });
+  } else {
+    Posts.insert(post)
+      .then(post => {
+        res.status(201).json({
+          success: true,
+          post
         });
-    }
-  });
+      })
+      .catch(err => {
+        res.status(500).json({
+          success: false,
+          errorMessage:
+            "There was an error while save the post to the database",
+          err
+        });
+      });
+  }
+});
 
 //Server.post id: comments
 router.post("/:id/comments", async (req, res) => {
@@ -163,33 +163,104 @@ router.post("/:id/comments", async (req, res) => {
 });
 
 //Server.delete posts
-router.delete('/:id', async (req, res) => {
-    const post_id = req.params.id;
-  
-    const post = await Posts.findById(post_id);
-  
-    if (post.length === 0) {
-      res.status(404).json({
-        success: false,
-        errorMessage: 'The post with the specificed ID does not exist.'
-      });
-    } else {
-      Posts.remove(post_id)
-        .then(post => {
-          res.status(200).json({
-            success: true,
-            post
-          });
-        })
-        .catch(err => {
-          res.status(500).json({
-            success: false,
-            errorMessage: 'The post could not be removed',
-            err
-          });
+router.delete("/:id", async (req, res) => {
+  const post_id = req.params.id;
+
+  const post = await Posts.findById(post_id);
+
+  if (post.length === 0) {
+    res.status(404).json({
+      success: false,
+      errorMessage: "The post with the specificed ID does not exist."
+    });
+  } else {
+    Posts.remove(post_id)
+      .then(post => {
+        res.status(200).json({
+          success: true,
+          post
         });
-    }
-  });
-  
-//Mvp
+      })
+      .catch(err => {
+        res.status(500).json({
+          success: false,
+          errorMessage: "The post could not be removed",
+          err
+        });
+      });
+  }
+});
+
+//Server.delete posts
+router.delete("/:id", async (req, res) => {
+  const post_id = req.params.id;
+
+  const post = await Posts.findById(post_id);
+
+  if (post.length === 0) {
+    res.status(404).json({
+      success: false,
+      errorMessage: "The post with the specificed ID does not exist."
+    });
+  } else {
+    Posts.remove(post_id)
+      .then(post => {
+        res.status(200).json({
+          success: true,
+          post
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          success: false,
+          errorMessage: "The post could not be removed",
+          err
+        });
+      });
+  }
+});
+
+//server.put
+router.put("/:id", async (req, res) => {
+  const post_id = req.params.id;
+  const updatedPost = req.body;
+
+  let existingPost = await Posts.findById(post_id);
+
+  if (existingPost.length === 0) {
+    res.status(404).json({
+      success: false,
+      errorMessage: "The post with the specificed ID does not exist."
+    });
+  }
+
+  if (!updatedPost.title || !updatedPost.contents) {
+    const missingField =
+      !updatedPost.title && !updatedPost.contents
+        ? "a title and contents"
+        : !updatedPost.title
+        ? "a title"
+        : "contents";
+
+    res.status(400).json({
+      success: false,
+      errorMessage: `Please provide ${missingField} for the post.`
+    });
+  } else {
+    Posts.update(post_id, updatedPost)
+      .then(post => {
+        res.status(200).json({
+          success: true,
+          post
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          success: false,
+          errorMessage: "The post could not be removed",
+          err
+        });
+      });
+  }
+});
 module.exports = router;
